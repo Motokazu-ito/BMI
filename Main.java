c341f9973e9530ca81714652d1dbe915308401a2
Motokazu Ito <m.itou.sys21@morijyobi.ac.jp>
2022-03-17 13:17:14 +0900

    BMI出力を2回になるよう修正しました:

--- Main.java
+++ Main.java
@@ -4,17 +4,20 @@ public class Main{
 		Scanner stdin=new Scanner(System.in);
 		double bmi;
 		double weight,height;
+		int a=0;
+		while(a<=2){
+		 System.out.println("体重を入力してください");
+		 weight=stdin.nextDouble();
 		
-		System.out.println("体重を入力してください");
-		weight=stdin.nextDouble();
-		
-		System.out.println("身長を入力してください");
-		height=stdin.nextDouble();
+		 System.out.println("身長を入力してください");
+		 height=stdin.nextDouble();
 
-		height=height/100;
-		bmi=weight/(height*height);
+		 height=height/100;
+		 bmi=weight/(height*height);
 
-		System.out.println("あなたのBMIは"+(int)bmi);
+		 System.out.println("あなたのBMIは"+(int)bmi);
+		 a++;
+		}
 	}
 }
 
